481d70bd99a253a86a2f9d71fbc7c74b7173f805
BeniBrandao <127801410+BeniBrandao@users.noreply.github.com>
2023-07-07 13:15:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+fabric.image.fromURL('aniverssarioImagem.png', function(Img) {
+blockImageObject= Img;
+blockImageObject.scaleToWidth(700);
+blockImageObject.scaleToblock(510);
+blockImageObject.set;({
+    top:0,
+    left:0
+});
+canvas.add(blockImageObject);
+});
+<buttom class="btn btn-warning btn-lg" onclick ="playSound()" >querida Mae</buttom>
+
+
+
+	
+
+
+}
+
+function playSound(){
+x.play();
+}
